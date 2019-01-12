856e6a5194e348f4772774e4606676cbd21368cc
Daniel Lenz <mail@daniellenz.org>
2019-01-11 16:15:26 -0800

    Add P14 CIB data, black formatting

--- cibinfo/fields.py
+++ cibinfo/fields.py
@@ -11,21 +11,22 @@ import pandas as pd
 
 from . import utils as ut
 from .powerspectra import (
-    cibxcib as TT, cibxphi as TK, phixphi as KK, noise)
+    cibxcib as TT,
+    cibxphi as TK,
+    phixphi as KK,
+    cmbxcmb as CC,
+    noise,
+)
 
 
 """Mapping of units of the CIB auto power spectrum (e.g. MJy^2/sr) to
 units of the cross power spectrum with the lensing (e.g. MJy).
 """
-unit_converter = {
-    "MJy^2/sr": "MJy",
-    "Jy^2/sr": "Jy",
-    "uK^2.sr": "uK",
-    "K^2.sr": "K",}
+unit_converter = {"MJy^2/sr": "MJy", "Jy^2/sr": "Jy", "uK^2.sr": "uK", "K^2.sr": "K"}
 
 
 class SimCl:
-    """Handles power spectra for the CIB and the lensing convergence kappa
+    """Handles power spectra for the CIB, CMB, and the lensing convergence kappa
     in a consistent manner.
     
     Examples
@@ -34,25 +35,30 @@ class SimCl:
     >> cl.tt
     >> cl.ell
     >> cl.noise_tt
+    >> cl.cc
     """
+
     _ell = None
 
-    _tt = None
-    _kk = None
-    _tk = None
+    _tt = None          # CIB Cl
+    _tt_data = None     # CIB P14 Cl data
+    _dtt_data = None    # CIB P14 Cl data
+    _cc = None          # CMB Cl
+    _kk = None          # Lensing convergence Cl
+    _tk = None          # Cl^CIBxKappa
 
-    _noise_tt = None
-    _noise_kk = None
+    _noise_tt = None  # Planck noise
+    _noise_kk = None  # Lensing convergence noise
 
-    def __init__(self, freq, unit='MJy^2/sr'):
+    def __init__(self, freq, unit="MJy^2/sr"):
         self.freq = freq
         self.unit = unit
-    
+
     @property
     def lmax_sim(self):
         """The maximum ell of all the different power spectrum models used
         here."""
-        return 1998
+        return 2500
 
     @property
     def ell(self):
@@ -61,50 +67,63 @@ class SimCl:
         return self._ell
 
     @property
+    def tt_data(self):
+        if self._tt_data is None:
+            self._tt_data = TT.Planck14Data(self.freq, unit=self.unit).Cl
+        return self._tt_data
+
+    @property
+    def dtt_data(self):
+        if self._dtt_data is None:
+            self._dtt_data = TT.Planck14Data(self.freq, unit=self.unit).dCl
+        return self._dtt_data
+
+    @property
     def tt(self):
         if self._tt is None:
-            self._tt = TT.Maniyar18Model(
-                self.freq, unit=self.unit).Cl
-            self._tt = np.concatenate((
-                [self._tt[0], self._tt[0]], self._tt,
-            ))
-        return self._tt[:self.lmax_sim]
+            self._tt = TT.Maniyar18Model(self.freq, unit=self.unit).Cl
+            self._tt = np.concatenate(([self._tt[0], self._tt[0]], self._tt))
+        return self._tt[: self.lmax_sim]
+
+    @property
+    def cc(self):
+        if self._cc is None:
+            self._cc = CC.Planck15Model(self.freq, unit=self.unit).Cl
+            self._cc = np.concatenate(([self._cc[0], self._cc[0]], self._cc))
+        return self._cc[: self.lmax_sim]
 
     @property
     def kk(self):
         if self._kk is None:
-            self._kk = KK.Planck18Kappa().Cl[:self.lmax_sim]
+            self._kk = KK.Planck18Kappa().Cl[: self.lmax_sim]
         return self._kk
 
     @property
     def tk(self):
         if self._tk is None:
-            self._tk = TK.Maniyar18Model(
-                self.freq,
-                unit=unit_converter[self.unit]).Cl
-            
+            self._tk = TK.Maniyar18Model(self.freq, unit=unit_converter[self.unit]).Cl
+
             # The monopole is missing, so we simply add one by hand
-            self._tk = np.concatenate((
-                [self._tk[0], ], self._tk
-            ))[:self.lmax_sim]
+            self._tk = np.concatenate(([self._tk[0]], self._tk))[: self.lmax_sim]
 
             # TK.Planck13Model() gives us the lensing potential Phi. We need
             # to convert this to the lensing convergence kappa.
-            self._tk *= ut.phi2kappa(np.arange(self.lmax_sim)) 
+            self._tk *= ut.phi2kappa(np.arange(self.lmax_sim))
 
         return self._tk
 
     @property
     def noise_tt(self):
         if self._noise_tt is None:
-            self._noise_tt = noise.PlanckPR3(
-                self.freq, unit=self.unit).Nl[:self.lmax_sim]
+            self._noise_tt = noise.PlanckPR3(self.freq, unit=self.unit).Nl[
+                : self.lmax_sim
+            ]
         return self._noise_tt
 
     @property
     def noise_kk(self):
         if self._noise_kk is None:
-            self._noise_kk = KK.Planck18Kappa().Nl[:self.lmax_sim]
+            self._noise_kk = KK.Planck18Kappa().Nl[: self.lmax_sim]
         return self._noise_kk
 
 
@@ -133,7 +152,7 @@ class Field:
             # maps
             self._lmax = min(1998, 3 * self.nside)
         elif not isinstance(value, int):
-            raise TypeError('lmax must be int or None.')
+            raise TypeError("lmax must be int or None.")
         else:
             self._lmax = value
 
@@ -220,7 +239,7 @@ class Field:
 #     @property
 #     def cib_alm(self):
 #         return self._cib_alm
-    
+
 #     @property
 #     def cib_map(self):
 #         return self._cib_map
@@ -236,10 +255,18 @@ class CorrField(Field):
     _Cl_XY = None
 
     def __init__(
-        self, freq, unit='MJy^2/sr', nside=1024,
-            pixwin1=False, fwhm1=0.0, add_noise1=False,
-            pixwin2=False, fwhm2=0.0, add_noise2=False,
-            lmax=None):
+        self,
+        freq,
+        unit="MJy^2/sr",
+        nside=1024,
+        pixwin1=False,
+        fwhm1=0.0,
+        add_noise1=False,
+        pixwin2=False,
+        fwhm2=0.0,
+        add_noise2=False,
+        lmax=None,
+    ):
 
         super().__init__(nside=nside, lmax=lmax)
 
@@ -279,25 +306,21 @@ class CorrField(Field):
 
     def generate_signal(self):
         self.alm_X, self.alm_Y = hp.synalm(
-            [
-                self.Cl_XX,
-                self.Cl_YY,
-                self.Cl_XY
-            ],
-            new=True)
+            [self.Cl_XX, self.Cl_YY, self.Cl_XY], new=True
+        )
 
     def generate_noise(self):
         if self.add_noise1:
             self.noisemap_X = hp.synfast(
-                self.Nl_XX, nside=self.nside,
-                pol=False, verbose=False,)
+                self.Nl_XX, nside=self.nside, pol=False, verbose=False
+            )
         else:
             self.noisemap_X = np.zeros(self.npix)
 
         if self.add_noise2:
             self.noisemap_Y = hp.synfast(
-                self.Nl_YY, nside=self.nside,
-                pol=False, verbose=False,)
+                self.Nl_YY, nside=self.nside, pol=False, verbose=False
+            )
         else:
             self.noisemap_Y = np.zeros(self.npix)
 
@@ -316,18 +339,26 @@ class CorrField(Field):
     @property
     def signalmap_X(self):
         self._signalmap_X = hp.alm2map(
-            self.alm_X, pixwin=self.pixwin1,
+            self.alm_X,
+            pixwin=self.pixwin1,
             fwhm=np.radians(self.fwhm1),
-            nside=self.nside, pol=False, verbose=False)
+            nside=self.nside,
+            pol=False,
+            verbose=False,
+        )
 
         return self._signalmap_X
 
     @property
     def signalmap_Y(self):
         self._signalmap_Y = hp.alm2map(
-            self.alm_Y, pixwin=self.pixwin2,
+            self.alm_Y,
+            pixwin=self.pixwin2,
             fwhm=np.radians(self.fwhm2),
-            nside=self.nside, pol=False, verbose=False)
+            nside=self.nside,
+            pol=False,
+            verbose=False,
+        )
 
         return self._signalmap_Y
 
@@ -338,35 +369,48 @@ class CIBxCIB(CorrField):
 
     """Generates two CIB fields with identical signal, but different noise
     properties."""
+
     def __init__(
-        self, freq, unit='MJy^2/sr', nside=1024, pixwin=True,
-        fwhm=0.0, add_noise=True, lmax=None):
+        self,
+        freq,
+        unit="MJy^2/sr",
+        nside=1024,
+        pixwin=True,
+        fwhm=0.0,
+        add_noise=True,
+        lmax=None,
+    ):
 
         super().__init__(
-            freq=freq, unit=unit, nside=nside,
-            pixwin1=pixwin, fwhm1=fwhm,
-            pixwin2=pixwin, fwhm2=fwhm,
-            add_noise1=add_noise, add_noise2=add_noise,
-            lmax=lmax
+            freq=freq,
+            unit=unit,
+            nside=nside,
+            pixwin1=pixwin,
+            fwhm1=fwhm,
+            pixwin2=pixwin,
+            fwhm2=fwhm,
+            add_noise1=add_noise,
+            add_noise2=add_noise,
+            lmax=lmax,
         )
 
-        self.sim_cl = SimCl(freq=freq, unit=unit) 
+        self.sim_cl = SimCl(freq=freq, unit=unit)
 
     @property
     def Cl_XX(self):
         if self._Cl_XX is None:
             self._Cl_XX = self.sim_cl.tt.copy()
 
-        return self._Cl_XX[:self.lmax]
+        return self._Cl_XX[: self.lmax]
 
     @property
     def Nl_XX(self):
         if self._Nl_XX is None:
             if self.add_noise1:
-                self._Nl_XX = self.sim_cl.noise_tt[:self.lmax]
+                self._Nl_XX = self.sim_cl.noise_tt[: self.lmax]
             else:
                 self._Nl_XX = np.zeros(self.lmax)
-        
+
         return self._Nl_XX
 
     @property
@@ -378,20 +422,16 @@ class CIBxCIB(CorrField):
         self._alm = val
 
     def generate_signal(self):
-        self.alm = hp.synalm(self.Cl_XX,)
+        self.alm = hp.synalm(self.Cl_XX)
 
     def generate_noise(self):
         if self.add_noise1:
-            self.noisemap_X = hp.synfast(
-                self.Nl_XX, nside=self.nside,
-                pol=False)
+            self.noisemap_X = hp.synfast(self.Nl_XX, nside=self.nside, pol=False)
         else:
             self.noisemap_X = np.zeros(self.npix)
 
         if self.add_noise2:
-            self.noisemap_Y = hp.synfast(
-                self.Nl_XX, nside=self.nside,
-                pol=False)
+            self.noisemap_Y = hp.synfast(self.Nl_XX, nside=self.nside, pol=False)
         else:
             self.noisemap_Y = np.zeros(self.npix)
 
@@ -410,9 +450,12 @@ class CIBxCIB(CorrField):
     @property
     def signalmap(self):
         self._signalmap = hp.alm2map(
-            self.alm, pixwin=self.pixwin1,
+            self.alm,
+            pixwin=self.pixwin1,
             fwhm=np.radians(self.fwhm1),
-            nside=self.nside, pol=False)
+            nside=self.nside,
+            pol=False,
+        )
 
         return self._signalmap
 
@@ -421,19 +464,30 @@ class CIBxKappa(CorrField):
     """Generates correlated fields of CIB and lensing convergence kappa."""
 
     def __init__(
-            self, freq, unit='MJy^2/sr', nside=1024,
-            pixwin_cib=False, fwhm_cib=0.0,
-            pixwin_lensing=False, fwhm_lensing=0.0,
-            add_cib_noise=False, add_lensing_noise=False,
-            lmax=None):
-
+        self,
+        freq,
+        unit="MJy^2/sr",
+        nside=1024,
+        pixwin_cib=False,
+        fwhm_cib=0.0,
+        pixwin_lensing=False,
+        fwhm_lensing=0.0,
+        add_cib_noise=False,
+        add_lensing_noise=False,
+        lmax=None,
+    ):
 
         super().__init__(
-            freq=freq, unit=unit, nside=nside,
-            pixwin1=pixwin_cib, fwhm1=fwhm_cib,
-            pixwin2=pixwin_lensing, fwhm2=fwhm_lensing,
-            add_noise1=add_cib_noise, add_noise2=add_lensing_noise,
-            lmax=lmax
+            freq=freq,
+            unit=unit,
+            nside=nside,
+            pixwin1=pixwin_cib,
+            fwhm1=fwhm_cib,
+            pixwin2=pixwin_lensing,
+            fwhm2=fwhm_lensing,
+            add_noise1=add_cib_noise,
+            add_noise2=add_lensing_noise,
+            lmax=lmax,
         )
 
         self.sim_cl = SimCl(freq=freq, unit=unit)
@@ -443,45 +497,46 @@ class CIBxKappa(CorrField):
         if self._Cl_XX is None:
             self._Cl_XX = self.sim_cl.tt.copy()
 
-        return self._Cl_XX[:self.lmax]
+        return self._Cl_XX[: self.lmax]
 
     @property
     def Cl_YY(self):
         if self._Cl_YY is None:
             self._Cl_YY = self.sim_cl.kk.copy()
 
-        return self._Cl_YY[:self.lmax]
+        return self._Cl_YY[: self.lmax]
 
     @property
     def Cl_XY(self):
         if self._Cl_XY is None:
             self._Cl_XY = self.sim_cl.tk.copy()
 
-        return self._Cl_XY[:self.lmax]
+        return self._Cl_XY[: self.lmax]
 
     @property
     def Nl_XX(self):
         if self._Nl_XX is None:
             if self.add_noise1:
-                self._Nl_XX = self.sim_cl.noise_tt[:self.lmax]
+                self._Nl_XX = self.sim_cl.noise_tt[: self.lmax]
             else:
                 self._Nl_XX = np.zeros(self.lmax)
-        
+
         return self._Nl_XX
 
     @property
     def Nl_YY(self):
         if self._Nl_YY is None:
             if self.add_noise2:
-                self._Nl_YY = self.sim_cl.noise_kk[:self.lmax]
+                self._Nl_YY = self.sim_cl.noise_kk[: self.lmax]
             else:
                 self._Nl_YY = np.zeros(self.lmax)
-        
+
         return self._Nl_YY
 
 
 def main():
     pass
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
