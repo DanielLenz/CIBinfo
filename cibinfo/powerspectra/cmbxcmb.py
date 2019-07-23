59c8ce0687e2841b10d738a308a16a0a9ecf389a
Daniel Lenz <mail@daniellenz.org>
2019-07-23 16:39:21 -0700

    Add errorbars to P18 CMB

--- cibinfo/powerspectra/cmbxcmb.py
+++ cibinfo/powerspectra/cmbxcmb.py
@@ -1,5 +1,4 @@
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 import numpy as np
 import pandas as pd
@@ -8,12 +7,10 @@ import os
 from .. import this_project as P
 
 
-__all__ = [
-    'Planck15Model',
-    'Planck18Data', ]
+__all__ = ["Planck15Model", "Planck18Data"]
 
 
-class CMBxCMB():
+class CMBxCMB:
 
     _l = None  # multipole
 
@@ -25,21 +22,19 @@ class CMBxCMB():
 
     _raw_table = None  # raw table, taken from publications, emails, etc
 
-    def __init__(self, freq=None, mode='TT', unit='uK^2.sr'):
-        if unit not in ['Jy^2/sr', 'MJy^2/sr', 'uK^2.sr', 'K^2.sr']:
+    def __init__(self, freq=None, mode="TT", unit="uK^2.sr"):
+        if unit not in ["Jy^2/sr", "MJy^2/sr", "uK^2.sr", "K^2.sr"]:
             raise ValueError(
-                'Unit must be either "Jy^2/sr",'
-                '"MJy^2/sr", "K^2.sr", or "uK^2.sr"')
+                'Unit must be either "Jy^2/sr",' '"MJy^2/sr", "K^2.sr", or "uK^2.sr"'
+            )
         self.unit = unit
 
-        if (unit not in ['uK^2.sr', 'K^2.sr']) and (freq is None):
-            raise ValueError(
-                'If unit is frequency-dependent, freq must be provided.')
+        if (unit not in ["uK^2.sr", "K^2.sr"]) and (freq is None):
+            raise ValueError("If unit is frequency-dependent, freq must be provided.")
 
         mode = mode.upper()
-        if mode not in ['TT', 'TE', 'EE', 'BB']:
-            raise ValueError(
-                "Mode must be in ['TT', 'TE', 'EE', 'BB']")
+        if mode not in ["TT", "TE", "EE", "BB"]:
+            raise ValueError("Mode must be in ['TT', 'TE', 'EE', 'BB']")
         self.mode = mode
 
         self.freq = freq
@@ -69,23 +64,25 @@ class CMBxCMB():
 
 
 class Planck15Model(CMBxCMB):
-    def __init__(self, freq=None, mode='TT', unit='uK^2.sr'):
-        super().__init__(
-            freq=freq, mode=mode, unit=unit)
+    def __init__(self, freq=None, mode="TT", unit="uK^2.sr"):
+        super().__init__(freq=freq, mode=mode, unit=unit)
 
     # Properties
     ############
     @property
     def raw_table(self):
         if self._raw_table is None:
-            _data = np.loadtxt(os.path.join(
-                P.PACKAGE_DIR,
-                'resources/cmbxcmb/COM_PowerSpect_CMB-base-plikHM-TT-lowTEB'
-                '-minimum-theory_R2.02.txt'))
+            _data = np.loadtxt(
+                os.path.join(
+                    P.PACKAGE_DIR,
+                    "resources/cmbxcmb/COM_PowerSpect_CMB-base-plikHM-TT-lowTEB"
+                    "-minimum-theory_R2.02.txt",
+                )
+            )
 
             self._raw_table = pd.DataFrame(
-                data=_data,
-                columns=['L', 'TT', 'TE', 'EE', 'BB', 'PP'])
+                data=_data, columns=["L", "TT", "TE", "EE", "BB", "PP"]
+            )
         return self._raw_table
 
     @property
@@ -96,7 +93,7 @@ class Planck15Model(CMBxCMB):
 
     @property
     def l_scaling(self):
-        return self.l*(self.l + 1) / 2. / np.pi
+        return self.l * (self.l + 1) / 2.0 / np.pi
 
     @property
     def l2Cl(self):
@@ -105,15 +102,15 @@ class Planck15Model(CMBxCMB):
             # Native unit is uK^2/sr
             self._l2Cl = self.raw_table[self.mode].values
 
-            if self.unit == 'K^2.sr':
-                self._l2Cl /= 1.e12
+            if self.unit == "K^2.sr":
+                self._l2Cl /= 1.0e12
 
-            if self.unit in ['MJy^2/sr', 'Jy^2/sr']:
-                self._l2Cl /= 1.e12  # from uK^2.sr to K^2.sr
-                self._l2Cl *= self.K2Jy[str(self.freq)]**2
+            if self.unit in ["MJy^2/sr", "Jy^2/sr"]:
+                self._l2Cl /= 1.0e12  # from uK^2.sr to K^2.sr
+                self._l2Cl *= self.K2Jy[str(self.freq)] ** 2
 
-            if self.unit == 'MJy^2/sr':
-                self._l2Cl /= 1.e12
+            if self.unit == "MJy^2/sr":
+                self._l2Cl /= 1.0e12
 
         return self._l2Cl
 
@@ -128,49 +125,60 @@ class Planck15Model(CMBxCMB):
 class Planck18Data(CMBxCMB):
     """Planck PR3 CMB TT power spectrum, as measured on the data.
     """
-    def __init__(self, freq=None, mode='TT', unit='uK^2.sr'):
-        super().__init__(
-            freq=freq, mode=mode, unit=unit)
+
+    def __init__(self, freq=None, mode="TT", unit="uK^2.sr"):
+        super().__init__(freq=freq, mode=mode, unit=unit)
 
     # Properties
     ############
     @property
     def raw_table(self):
         if self._raw_table is None:
-            self._raw_table = np.loadtxt(os.path.join(
-                P.PACKAGE_DIR,
-                'resources/cmbxcmb/COM_PowerSpect_CMB-TT-full_R3.01.txt'))
+            self._raw_table = pd.read_fwf(
+                os.path.join(
+                    P.PACKAGE_DIR,
+                    "resources/cmbxcmb/COM_PowerSpect_CMB-TT-full_R3.01.txt",
+                ),
+                comment="#",
+            )
 
         return self._raw_table
 
     @property
     def l(self):
         if self._l is None:
-            self._l = self.raw_table[:, 0]
+            self._l = self.raw_table["l"]
         return self._l
 
     @property
     def l_scaling(self):
-        return self.l*(self.l + 1) / 2. / np.pi
+        return self.l * (self.l + 1) / 2.0 / np.pi
 
     @property
     def l2Cl(self):
         """l(l+1)/2pi * Cl"""
         if self._l2Cl is None:
             # Native unit is uK^2/sr
-            self._l2Cl = self.raw_table[:, 1]
+            self._l2Cl = self.raw_table["Dl"]
 
-            if self.unit == 'K^2.sr':
-                self._l2Cl /= 1.e12
+            if self.unit == "K^2.sr":
+                self._l2Cl /= 1.0e12
 
-            if self.unit in ['MJy^2/sr', 'Jy^2/sr']:
-                self._l2Cl /= 1.e12  # from uK^2.sr to K^2.sr
-                self._l2Cl *= self.K2Jy[str(self.freq)]**2
+            if self.unit in ["MJy^2/sr", "Jy^2/sr"]:
+                self._l2Cl /= 1.0e12  # from uK^2.sr to K^2.sr
+                self._l2Cl *= self.K2Jy[str(self.freq)] ** 2
 
-            if self.unit == 'MJy^2/sr':
-                self._l2Cl /= 1.e12
+            if self.unit == "MJy^2/sr":
+                self._l2Cl /= 1.0e12
 
         return self._l2Cl
+
+    @property
+    def dl2Cl(self):
+        if self._dl2Cl is None:
+            self._dl2Cl = np.row_stack((self.raw_table["-dDl"], self.raw_table["+dDl"]))
+
+        return self._dl2Cl
 
     @property
     def Cl(self):
@@ -178,3 +186,10 @@ class Planck18Data(CMBxCMB):
             self._Cl = self.l2Cl / self.l_scaling
 
         return self._Cl
+
+    @property
+    def dCl(self):
+        if self._dCl is None:
+            self._dCl = self.dl2Cl / self.l_scaling
+
+        return self._dCl
